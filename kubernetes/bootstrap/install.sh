e1a7de3f4cffb5e3f3082953fbf0bae936871917
José Miguel Silva Caldeira <miguel@ncdc.pt>
2023-10-22 00:21:51 +0100

    Add some checks to executed commands and cleanup

--- kubernetes/bootstrap/install.sh
+++ kubernetes/bootstrap/install.sh
@@ -1,12 +1,12 @@
-# This script has been tested on Ubuntu 20.04
+# This script has been tested on Ubuntu 18.04, 20.04, 22.04
 # For other versions of Ubuntu, you might need some tweaking
 
 echo "Install essential packages"
-apt install -qq -y net-tools curl ssh software-properties-common >/dev/null
+apt install -qq -y net-tools curl ssh software-properties-common
 
 echo "Install containerd runtime"
-apt update -qq 2>/dev/null
-apt install -qq -y containerd apt-transport-https 2>/dev/null
+apt update -qq
+apt install -qq -y containerd apt-transport-https
 echo return: $?
 
 
@@ -20,11 +20,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 echo "Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=${K8S_VERSION}-00 kubelet=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00  2>/dev/null
+apt install -qq -y kubeadm=${K8S_VERSION}-00 kubelet=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00
 echo return: $?
 
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 systemctl restart kubelet
 
-apt autoremove -y  >/dev/null
+apt autoremove -y
 
