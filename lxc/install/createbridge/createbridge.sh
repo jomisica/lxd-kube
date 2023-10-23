cc2be4923a1b5e66e02caf929ef29495ff619516
José Miguel Silva Caldeira <miguel@ncdc.pt>
2023-10-23 19:38:03 +0100

    up

---
+++ lxc/install/createbridge/createbridge.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    echo "Example: sudo bash install.sh"
+    exit
+fi
+
+# verificar se temos o comando brctl
+if ! [ -x "$(command -v brctl)" ]; then
+    echo "Error: brctl is not installed." >&2
+    echo "Instalando o pacote necessário."
+    apt-get install bridge-utils >&2
+fi
+
+# Verificar se já temeos a bridge "lxdbridge"
+brctl show lxdbridge >&2
+if [ $? -eq 0 ]; then
+    echo "[OK] A bridge 'lxdbridge' já existe."
+else
+    echo "[OK] Adicionando a bridge 'lxdbridge'."
+    brctl addbr lxdbridge >&2
+
+    brctl show lxdbridge >&2
+    if [ $? -eq 0 ]; then
+        echo "[OK] A bridge 'lxdbridge' já existe."
+    else
+        echo "[ERROR] Não foi possivel criar a bridge 'lxdbridge'."
+        exit 255
+    fi
+fi
+
+# get default interface
+export DEFAULT_IF=$(/sbin/ip route | awk '/^default/ { print $5 }')
+
+brctl addif lxdbridge "${DEFAULT_IF}"
+
+# netplan config file
+CONFIG_NETPLAN_FILE=$(grep -r "${DEFAULT_IF}:" /etc/netplan/ | awk '{sub(/:.*/, ""); print}')
+if [ $? -eq 0 ]; then
+    echo "[OK] A bridge 'lxdbridge' já existe."
+fi
+
+$(envsubst <lxc/install/createbridge/netplancfg.yaml > "${CONFIG_NETPLAN_FILE}"
+
+netplan apply
+
+exit
