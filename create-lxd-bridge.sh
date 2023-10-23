35454ffc6ce4b56cad282f68cdbbeccbbb28153f
José Miguel Silva Caldeira <miguel@ncdc.pt>
2023-10-23 20:37:39 +0100

    update messages

--- create-lxd-bridge.sh
+++ create-lxd-bridge.sh
@@ -6,26 +6,29 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-# verificar se temos o comando brctl
+# Check if we have the brctl command
 if ! [ -x "$(command -v brctl)" ]; then
-    echo "Error: brctl is not installed." >&2
-    echo "Instalando o pacote necessário."
+    echo "Installing the required bridge-utils package."
     apt-get install bridge-utils >&2
+    if ! [ -x "$(command -v brctl)" ]; then
+        echo "Error: brctl is not installed." >&2
+        exit 255
+    fi
 fi
 
-# Verificar se já temeos a bridge "lxdbridge"
+# Check if we already have the "lxdbridge" bridge
 brctl show lxdbridge >&2
 if [ $? -eq 0 ]; then
-    echo "[OK] A bridge 'lxdbridge' já existe."
+    echo "[ON] The bridge 'lxdbridge' already exists."
 else
-    echo "[OK] Adicionando a bridge 'lxdbridge'."
+    echo "[OK] Adding bridge 'lxdbridge'."
     brctl addbr lxdbridge >&2
 
     brctl show lxdbridge >&2
     if [ $? -eq 0 ]; then
-        echo "[OK] A bridge 'lxdbridge' já existe."
+        echo "[ON] The bridge 'lxdbridge' already exists."
     else
-        echo "[ERROR] Não foi possivel criar a bridge 'lxdbridge'."
+        echo "[ERROR] Unable to create bridge 'lxdbridge'."
         exit 255
     fi
 fi
@@ -33,8 +36,8 @@ fi
 # get default interface
 export DEFAULT_IF=$(/sbin/ip route | awk '/^default/ { print $5 }')
 if [ "${DEFAULT_IF}" = "lxdbridge" ]; then
-    echo "[ERROR] A rota por defeito já é para a bridge 'lxdbridge'."
-    echo "[ABORT] A bridge parece estar configurada 'lxdbridge'."
+    echo "[ERROR] The default route is already for bridge 'lxdbridge'."
+    echo "[ABORT] The bridge appears to be configured 'lxdbridge'."
     exit 255
 fi
 
@@ -42,19 +45,22 @@ brctl addif lxdbridge "${DEFAULT_IF}"
 
 brctl show lxdbridge | grep "${DEFAULT_IF}"
 if [ $? -eq 0 ]; then
-    echo "[OK] A interface '${DEFAULT_IF}' foi adicionada à bridge 'lxdbridge'."
+    echo "[OK] Interface '${DEFAULT_IF}' has been added to bridge 'lxdbridge'."
 else
-    echo "[ERROR] Não foi possivel a interface '${DEFAULT_IF}' à bridge 'lxdbridge'."
+    echo "[ERROR] Unable to add interface '${DEFAULT_IF}' to bridge 'lxdbridge'."
     exit 255
 fi
 
 # netplan config file
 CONFIG_NETPLAN_FILE=$(grep -r "${DEFAULT_IF}:" /etc/netplan/ | awk '{sub(/:.*/, ""); print}')
 
+# Backup of the current netplan file
 cp "${CONFIG_NETPLAN_FILE}" /root
 
+# resolve temple
 $(envsubst <lxc/netplan/netplancfg.yaml >"${CONFIG_NETPLAN_FILE}")
 
+# apply netplan configuration
 netplan apply
 
 exit
