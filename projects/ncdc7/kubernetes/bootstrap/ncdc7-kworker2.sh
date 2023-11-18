05376bf1fb043912f590f995f96e31824b1e2d80
José Miguel Silva Caldeira <miguel@ncdc.pt>
2023-11-18 18:22:22 +0000

    ha tests

---
+++ projects/ncdc7/kubernetes/bootstrap/ncdc7-kworker2.sh
@@ -0,0 +1,49 @@
+set -o xtrace
+
+env
+
+# This script has been tested on Ubuntu 18.04, 20.04, 22.04
+# For other versions of Ubuntu, you might need some tweaking
+
+echo "Install essential packages"
+#apt install -qq -y net-tools curl ssh software-properties-common 
+
+echo "Install runtime"
+apt update -qq
+apt install -qq -y apt-transport-https ca-certificates net-tools curl
+echo return: $?
+
+
+source /etc/os-release
+OS=x${NAME}_${VERSION_ID}
+CRIO_VERSION=${K8S_VERSION%.*}
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | sudo apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
+apt update -y
+apt install cri-o cri-o-runc -y
+systemctl enable crio.service
+systemctl start crio.service
+apt install cri-tools -y
+
+
+
+#curl -s https://packages.cloud.google.com/apt/dists/kubernetes-xenial/main/binary-amd64/Packages | grep Version | awk '{print $2}'
+echo "Add apt repo for kubernetes"
+curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt update -qq
+
+echo "Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt install -qq -y kubeadm=${K8S_VERSION}-00 kubelet=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00
+echo return: $?
+apt-mark hold kubelet kubeadm kubectl
+
+echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
+systemctl restart kubelet
+
+apt autoremove -y
---
+++ projects/ncdc7/kubernetes/bootstrap/proxy.sh
@@ -0,0 +1,106 @@
+set -o xtrace
+
+env
+
+
+
+apt update && apt install -y keepalived haproxy curl
+
+
+cat > /etc/keepalived/check_apiserver.sh <<EOF
+#!/bin/sh
+
+errorExit() {
+  echo "*** $@" 1>&2
+  exit 1
+}
+
+curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
+if ip addr | grep -q ${K8S_KUBE_CONTROLPLANE_ENDPOINT_VIRTUAL_IP}; then
+  curl --silent --max-time 2 --insecure https://${K8S_KUBE_CONTROLPLANE_ENDPOINT_VIRTUAL_IP}:6443/ -o /dev/null || errorExit "Error GET https://${K8S_KUBE_CONTROLPLANE_ENDPOINT_VIRTUAL_IP}:6443/"
+fi
+EOF
+
+chmod +x /etc/keepalived/check_apiserver.sh
+
+cat > /etc/haproxy/haproxy.cfg <<EOF
+global
+	log /dev/log	local0
+	log /dev/log	local1 notice
+	chroot /var/lib/haproxy
+	stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
+	stats timeout 30s
+	user haproxy
+	group haproxy
+	daemon	# Default SSL material locations
+	ca-base /etc/ssl/certs
+	crt-base /etc/ssl/private
+	# See: https://ssl-config.mozilla.org/#server=haproxy&server-version=2.0.3&config=intermediate
+	ssl-default-bind-ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384
+	ssl-default-bind-ciphersuites TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256
+	ssl-default-bind-options ssl-min-ver TLSv1.2 no-tls-tickets
+defaults
+	log	global
+	mode	http
+	option	httplog
+	option	dontlognull
+	timeout connect 5000
+	timeout client  50000
+	timeout server  50000
+	errorfile 400 /etc/haproxy/errors/400.http
+	errorfile 403 /etc/haproxy/errors/403.http
+	errorfile 408 /etc/haproxy/errors/408.http
+	errorfile 500 /etc/haproxy/errors/500.http
+	errorfile 502 /etc/haproxy/errors/502.http
+	errorfile 503 /etc/haproxy/errors/503.http
+	errorfile 504 /etc/haproxy/errors/504.http
+
+frontend kube-apiserver
+  bind *:6443
+  mode tcp
+  option tcplog
+  default_backend kube-apiserver
+
+backend kube-apiserver
+    mode tcp
+    option tcplog
+    option tcp-check      
+    balance roundrobin
+    default-server inter 10s downinter 5s rise 2 fall 2 slowstart 60s maxconn 250 maxqueue 256 weight 100
+    #server ncdc7-kmaster1 192.168.16.157:6443 check
+    #server ncdc7-kmaster2 192.168.16.158:6443 check
+${CONTROLPLANES_INSTANCES_DATA}
+EOF
+
+cat > /etc/keepalived/keepalived.conf <<EOF
+vrrp_script check_apiserver {
+  script "/etc/keepalived/check_apiserver.sh"
+  interval 3
+  timeout 10
+  fall 5
+  rise 2
+  weight -2
+}
+
+vrrp_instance VI_1 {
+    state BACKUP
+    interface eth0
+    virtual_router_id 1
+    priority 100
+    advert_int 5
+    authentication {
+        auth_type PASS
+        auth_pass mysecret
+    }
+    virtual_ipaddress {
+        ${K8S_KUBE_CONTROLPLANE_ENDPOINT_VIRTUAL_IP}
+    }
+    track_script {
+        check_apiserver
+    }
+}
+EOF
+
+systemctl enable haproxy && systemctl restart haproxy
+systemctl enable keepalived && systemctl restart keepalived
+
